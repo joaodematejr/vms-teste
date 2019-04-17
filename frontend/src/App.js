b63541ff85998f6e7e207a8d2c30f701349493d8
joaodematejr <joaodematejr@gmail.com>
2019-04-17 14:29:37 -0300

    att

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,60 @@
+import { TextField, Grid, Button, Link } from '@material-ui/core';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const MyLink = props => <Link to="/open-collective" {...props} />
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="p-12 sm:p-24 max-w-2xl">
+              <Grid container spacing={24}>
+                <Grid item sm={3}>
+                  <TextField
+                    style={{ marginLeft: '1%', marginTop: '5%' }}
+                    id="cameraDescricao"
+                    name="cameraDescricao"
+                    fullWidth
+                    variant="outlined"
+                    onChange={this.handleChange}
+                    label="Descrição da Câmera"
+                  />
+                </Grid>
+                <Grid item sm={3}>
+                  <TextField
+                    style={{ marginTop: '5%' }}
+                    id="cameraIp"
+                    name="cameraIp"
+                    fullWidth
+                    variant="outlined"
+                    onChange={this.handleChange}
+                    label="Ip da Câmera"
+                  />
+                </Grid>
+                <Grid item sm={3}>
+                  <TextField
+                    style={{ marginTop: '5%' }}
+                    id="cameraPorta"
+                    name="cameraPorta"
+                    fullWidth
+                    variant="outlined"
+                    onChange={this.handleChange}
+                    label="Porta da Câmera"
+                  />
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+          <div className="row">
+            <div className="p-12 sm:p-24 max-w-2xl">
+              <Grid item sm={3} style={{ marginTop: '1%' }}>
+                <Button component={MyLink}>Enviar</Button>
+              </Grid>
+            </div>
+          </div>
+        </form>
       </div>
     );
   }
